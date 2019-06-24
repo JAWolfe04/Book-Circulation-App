6db7ef462bd0730ae1f79256a5d8c5cb56d9a08a
JAWolfe04 <jawhf4@mail.umkc.edu>
2019-06-24 14:43:50 -0500

    Reviewed Employee class adding documentation (#1)
    Removed empty default constructor, only need one default constructor. Removed iostream as it is not needed here. Added some formatting and documentation.

--- Employee.h
+++ Employee.h
@@ -1,31 +1,26 @@
 #pragma once
-#include <iostream>
 #include <string>
 
 using namespace std;
+
+// Manages employee name, book waiting time and retention time
 class Employee
 {
 private:
-	string EmployeeName;
-	int WaitingTime;
-	int RetainingTime;
+    string EmployeeName; // Name of the employee
+    int WaitingTime; // Days the employee waited to get a book since circulation began
+    int RetainingTime; // Days the employee retained books
 
 public:
-	Employee();
-	Employee() {
-		EmployeeName = "";
-		WaitingTime = 0;
-		RetainingTime = 0;
-	}
+    // Default constructor sets to empty name and 
+    // both waiting and retaining times to 0
+    Employee() : EmployeeName(""), WaitingTime(0), RetainingTime(0) { }
 
-	void setName(string name){ EmployeeName = name; }      //inline getters and setters
-	void setWaitTime(int wtime) {	WaitingTime = wtime;}
-	void setRetainingTime(int rtime) { RetainingTime = rtime; }
+    inline void setName(string name) { EmployeeName = name; }
+    inline void setWaitTime(int wtime) { WaitingTime = wtime; }
+    inline void setRetainingTime(int rtime) { RetainingTime = rtime; }
 
-	string getName() { return EmployeeName; }
-	int getWaitTime() { return WaitingTime; }
-	int getRetainingTime() { return RetainingTime; }
-
-	//~Employee();
-
+    inline string getName() { return EmployeeName; }
+    inline int getWaitTime() { return WaitingTime; }
+    inline int getRetainingTime() { return RetainingTime; }
 };
