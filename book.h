9b8e7accbaf882e77e8deeefb5ea32394c70134f
Conraw <46946984+Conraw@users.noreply.github.com>
2019-06-25 16:40:59 -0500

    Create book.h

---
+++ book.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class book
+{
+private:
+	bool is_archived;
+	string StartDate;
+	string EndDate;
+
+public:
+	book() : is_archived(true), StartDate(""), EndDate("") { }
+
+	inline void setStartDate(string sDate) { StartDate = sDate; }
+	inline void setEndDate(string eDate) { EndDate = eDate; }
+
+	bool Is_Archived(string book);
+
+	inline string getEndDate() { return EndDate; }
+	inline string getStartDate() { return StartDate; }
+	inline bool Is_Archived() { return is_archived; }
+
+
+
+	~book();
+};
+
