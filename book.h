5755ce353e19f32cc0a86f24a0c961b3dbf5385b
Conraw <46946984+Conraw@users.noreply.github.com>
2019-06-25 16:44:35 -0500

    Update book.h

--- book.h
+++ book.h
@@ -6,22 +6,24 @@ using namespace std;
 class book
 {
 private:
+	string BookName;
 	bool is_archived;
 	string StartDate;
 	string EndDate;
 
 public:
-	book() : is_archived(true), StartDate(""), EndDate("") { }
+	book() : BookName(""), is_archived(true), StartDate(""), EndDate("") { }
 
+	inline void setBookName(string bookname) { BookName = BookName;  }
 	inline void setStartDate(string sDate) { StartDate = sDate; }
 	inline void setEndDate(string eDate) { EndDate = eDate; }
 
 	//bool Is_Archived(string book);
 
+	inline string getBookName() { return BookName; }
 	inline string getEndDate() { return EndDate; }
 	inline string getStartDate() { return StartDate; }
 	inline bool Is_Archived(string book) { return is_archived; }
 
 };
-
 
