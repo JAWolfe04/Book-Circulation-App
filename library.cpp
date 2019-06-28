35a3ab095dd7fcbb6951a3ee67b89bc2fcfbe870
Conraw <46946984+Conraw@users.noreply.github.com>
2019-06-27 22:23:48 -0500

    Create library.cpp

---
+++ library.cpp
@@ -0,0 +1,41 @@
+#include "library.h"
+#include <iterator>
+#include <algorithm>
+
+
+void library::add_book(string bookname)
+{
+	Circulated_Book.push_back(book(bookname));
+}
+
+void library::add_employee(string employeename)
+{
+	Employees.push_back(Employee(employeename));
+}
+
+string library::getBook(string bookname)
+{
+	/*list<string>::iterator it;
+
+	it = std::find(Circulated_Book.begin(), book.end(), bookname);
+
+	return *it;*/
+
+	std::list<book>::iterator result = Circulated_Book.begin();
+	while (true) {
+		result = std::find_if(result, Circulated_Book.end(), std::bind2nd(BookNumber(), number));
+		if (result == Circulated_Book.end())
+			break;
+
+	}
+	return *result;
+	
+}
+
+void library::circulate_book(string bookname, int date)
+{
+}
+
+void library::pass_on(string bookname, int date)
+{
+}
