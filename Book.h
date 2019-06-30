76075529110c4fa26f575919680876af8fe64728
Jonathan <jawhf4@mail.umkc.edu>
2019-06-30 16:33:54 -0500

    Removed all tabs for spaces in project

--- Book.h
+++ Book.h
@@ -9,44 +9,44 @@
 class Book
 {
 private:
-	std::string book_name;
-	bool is_archived;
-	Date start_date;
-	Date last_passed_date;
-	Date end_date;
-	priority_queue<Employee> employee_queue;
+    std::string book_name;
+    bool is_archived;
+    Date start_date;
+    Date last_passed_date;
+    Date end_date;
+    priority_queue<Employee> employee_queue;
 
 public:
-	Book() : book_name(""), is_archived(false) { }
-	Book(string& book_name) : book_name(book_name), is_archived(false) { }
+    Book() : book_name(""), is_archived(false) { }
+    Book(string& book_name) : book_name(book_name), is_archived(false) { }
 
-	//Setters for book name, start date, date that the book was last passed, and end date
-	inline void set_book_name(std::string& book_name) { this->book_name = book_name; }
-	inline void set_start_date(Date& start_date) { this->start_date = start_date; }
-	inline void set_last_passed_date(Date& pass_date) { this->last_passed_date = pass_date; }
-	inline void set_end_date(Date& end_date) { this->end_date = end_date; }
+    //Setters for book name, start date, date that the book was last passed, and end date
+    inline void set_book_name(std::string& book_name) { this->book_name = book_name; }
+    inline void set_start_date(Date& start_date) { this->start_date = start_date; }
+    inline void set_last_passed_date(Date& pass_date) { this->last_passed_date = pass_date; }
+    inline void set_end_date(Date& end_date) { this->end_date = end_date; }
 
-	//Used in the process of archiving the book - sets the archive variable to true
-	inline void archive() { this->is_archived = true; }
+    //Used in the process of archiving the book - sets the archive variable to true
+    inline void archive() { this->is_archived = true; }
 
-	//Getters for book name, start date, date that the book was last passed, and end date
-	inline std::string& get_book_name() { return book_name; }
-	inline Date& get_end_date() { return end_date; }
-	inline Date& get_last_pass_date() { return last_passed_date; }
-	inline Date& get_start_date() { return start_date; }
+    //Getters for book name, start date, date that the book was last passed, and end date
+    inline std::string& get_book_name() { return book_name; }
+    inline Date& get_end_date() { return end_date; }
+    inline Date& get_last_pass_date() { return last_passed_date; }
+    inline Date& get_start_date() { return start_date; }
 
-	//Returns whether the book is archived currently
-	inline bool is_book_archived() { return is_archived; }
+    //Returns whether the book is archived currently
+    inline bool is_book_archived() { return is_archived; }
 
-	//overloaded equality operator, determines whether the book's name is equal to a given string
-	inline bool operator==(std::string name) { return book_name == name; }
+    //overloaded equality operator, determines whether the book's name is equal to a given string
+    inline bool operator==(std::string name) { return book_name == name; }
     friend bool operator==(const Book& lhs, const Book& rhs) { return lhs.book_name == rhs.book_name; }
 
-	//Priority queue functions used on the employee queue
-	void push_employee(Employee& employee) { employee_queue.push(employee); }
-	void pop_employee() { employee_queue.pop(); }
-	void update_employee(Employee& employee) { employee_queue.update_item(employee); }
-	Employee front_employee() { return employee_queue.front(); }
-	bool is_empty() { return (employee_queue.isEmpty()); }
+    //Priority queue functions used on the employee queue
+    void push_employee(Employee& employee) { employee_queue.push(employee); }
+    void pop_employee() { employee_queue.pop(); }
+    void update_employee(Employee& employee) { employee_queue.update_item(employee); }
+    Employee front_employee() { return employee_queue.front(); }
+    bool is_empty() { return (employee_queue.isEmpty()); }
 
 };
--- Library.cpp
+++ Library.cpp
@@ -9,86 +9,86 @@
 //Efficiency: O(n) (see priority queue)
 void Library::add_book(std::string book_name)
 {
-	circulated_books.push_back(Book(book_name));
+    circulated_books.push_back(Book(book_name));
 }
 
 //Efficiency: O(n) (see priority queue)
 //Adds new employee to the employee list
 void Library::add_employee(std::string employee_name)
 {
-	employees.push_back(Employee(employee_name));
+    employees.push_back(Employee(employee_name));
 }
 
 //Searches the circulated book list for a book and returns a pointer to it if found
 //Efficiency: O(n) (the iterator goes through n books to find the book)
 Book* Library::get_book(std::string book_name)
 {
-	std::list<Book>::iterator itr = find(circulated_books.begin(), circulated_books.end(), book_name);
+    std::list<Book>::iterator itr = find(circulated_books.begin(), circulated_books.end(), book_name);
 
-	if (itr == circulated_books.end())
-		throw std::exception("No book of that name found");
-	else
-		return &(*itr);
+    if (itr == circulated_books.end())
+        throw std::exception("No book of that name found");
+    else
+        return &(*itr);
 }
 
 //Searches the employee list for an employee and returns a pointer to it if found
 //Efficiency: O(n) (the iterator goes through n employees to find the employee)
 Employee* Library::get_employee(Employee& employee)
 {
-	std::list<Employee>::iterator itr = find(employees.begin(), employees.end(), employee);
+    std::list<Employee>::iterator itr = find(employees.begin(), employees.end(), employee);
 
-	if (itr == employees.end())
-		throw std::exception("No employee of that name found");
-	else
-		return &(*itr);
+    if (itr == employees.end())
+        throw std::exception("No employee of that name found");
+    else
+        return &(*itr);
 }
 
 //Puts book in circulation by setting the start date and adding all employees to the book's queue
 //Efficiency: O(n)
 void Library::circulate_book(std::string book_name, Date date)
 {
-	Book* book = get_book(book_name);
-	book->set_start_date(date);
-	book->set_last_passed_date(date);
+    Book* book = get_book(book_name);
+    book->set_start_date(date);
+    book->set_last_passed_date(date);
 
-	std::list<Employee>::iterator itr = employees.begin();
-	for (itr; itr != employees.end(); ++itr)
-		book->push_employee(*itr);
+    std::list<Employee>::iterator itr = employees.begin();
+    for (itr; itr != employees.end(); ++itr)
+        book->push_employee(*itr);
 }
 
 //Passes book from one employee to the next
 //Efficiency: O(n^2)
 void Library::pass_on(std::string book_name, Date date)
 {
-	Book* book = get_book(book_name);
-	Employee front = book->front_employee();
-	Employee* front_employee = get_employee(front);
+    Book* book = get_book(book_name);
+    Employee front = book->front_employee();
+    Employee* front_employee = get_employee(front);
     int retain_time = (date - book->get_last_pass_date()) + front_employee->getRetainingTime();
-	front_employee->setRetainingTime(retain_time); //update retaining time for employee who last had book
+    front_employee->setRetainingTime(retain_time); //update retaining time for employee who last had book
     book->set_last_passed_date(date);
-	book->pop_employee();
+    book->pop_employee();
 
-	//update employee's retaining time in each book's queue
-	for (std::list<Book>::iterator itr = circulated_books.begin(); itr != circulated_books.end(); ++itr) {
-		itr->update_employee(*front_employee);
-	}
+    //update employee's retaining time in each book's queue
+    for (std::list<Book>::iterator itr = circulated_books.begin(); itr != circulated_books.end(); ++itr) {
+        itr->update_employee(*front_employee);
+    }
 
-	//if book's queue is empty, archive the book and exit
-	if (book->is_empty()) {
-		book->archive();
-		book->set_end_date(date);
+    //if book's queue is empty, archive the book and exit
+    if (book->is_empty()) {
+        book->archive();
+        book->set_end_date(date);
         archived_books.push_back(*book);
-		circulated_books.remove(*book);
-		return;
-	}
+        circulated_books.remove(*book);
+        return;
+    }
 
-	//Set new front employee's wait time and update in each book
-	Employee front2 = book->front_employee();
+    //Set new front employee's wait time and update in each book
+    Employee front2 = book->front_employee();
     front = book->front_employee();
-	Employee* front_employee2 = get_employee(front);
+    Employee* front_employee2 = get_employee(front);
     int wait_time = (date - book->get_start_date()) + front_employee2->getWaitTime();
-	front_employee2->setWaitTime(wait_time);
-	for (std::list<Book>::iterator itr = circulated_books.begin(); itr != circulated_books.end(); ++itr) {
-		itr->update_employee(*front_employee2);
-	}
+    front_employee2->setWaitTime(wait_time);
+    for (std::list<Book>::iterator itr = circulated_books.begin(); itr != circulated_books.end(); ++itr) {
+        itr->update_employee(*front_employee2);
+    }
 }
